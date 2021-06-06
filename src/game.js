6fd3960713f8786d145f91827cc391d77a348e91
Maria <mafemont16@gmail.com>
2021-06-06 09:36:32 +0200

    changed the size of the sprites of the character

--- src/game.js
+++ src/game.js
@@ -1,7 +1,7 @@
 
 var config = {
     type: Phaser.AUTO,
-    //backgroundColor: '#f1faee',
+    backgroundColor: '#333333',
     physics: {
         default: 'arcade',
         arcade: {
@@ -10,20 +10,20 @@ var config = {
         }
     },
     scale: {
-        mode: Phaser.Scale.FIT,
-        parent: 'phaser-example',
-        autoCenter: Phaser.Scale.CENTER_BOTH,
+        mode: Phaser.Scale.RESIZE,
+        //parent: 'phaser-example',
     },
     scene: {
         preload: preload,
         create: create,
         update: update
-    },
+    }
 };
 var game = new Phaser.Game(config);
 
 var player;
 var masks;
+var bags;
 var platforms;
 var cursors;
 var score = 0;
@@ -36,7 +36,7 @@ var textGameOver;
 var timedEvent;
 var gameStarted;
 var finishedGame;
-var dudito;
+//var dudito;
 
 /*function gameScene() {
     this.score = 0;
@@ -44,18 +44,17 @@ var dudito;
 }*/
 
 function preload() {
-    this.load.image('sky', '/assets/images/sky1.png');
+    this.load.image('sky', '/assets/images/city.png');
     this.load.image('groundBase', '/assets/images/platform-base.png');
     this.load.image('ground', '/assets/images/platform.png');
     this.load.image('platformAir', '/assets/images/platform-air.png');
-    this.load.image('mushroomRed', '/assets/images/mushroom_red.png');
-    this.load.image('mushroomBrown', '/assets/images/mushroom_brown.png');
-    this.load.image('pitillo', '/assets/images/pitillo.png');
+    this.load.image('trashcan', '/assets/images/trashcan.png');
+    this.load.image('win', '/assets/images/win.png');
     this.load.image('mask1', '/assets/images/mask1.png');
-    this.load.image('sac', '/assets/images/sac.png');
+    this.load.image('bags', '/assets/images/plastic-bag.png');
     this.load.image('mask', '/assets/images/mask.png');
-    this.load.spritesheet('dude', '/assets/images/dude.png',
-        { frameWidth: 32, frameHeight: 48 });
+    this.load.spritesheet('dude', '/assets/images/dudek1.png',
+        { frameWidth: 72, frameHeight: 95 });
 }
 
 function create() {
@@ -63,30 +62,19 @@ function create() {
     this.add.image(500, 400, 'sky');
 
     platforms = this.physics.add.staticGroup();
-    platforms.create(700, 810, 'groundBase').setScale(1).refreshBody();
+    platforms.create(650, 780, 'groundBase').setScale(1).refreshBody();
     platforms.create(550, 490, 'ground');
     platforms.create(50, 250, 'ground');
-    platforms.create(50, 500, 'platformAir');
+    //platforms.create(50, 500, 'platformAir');
+    //platforms.create(900, 220, 'ground');
     platforms.create(900, 220, 'ground');
-    platforms.create(900, 220, 'ground');
-    platforms.create(150, 680, 'mushroomRed');
+    platforms.create(750, 720, 'trashcan');
 
-
-    player = this.physics.add.sprite(200, 400, 'dude');
+    player = this.physics.add.sprite(200, 420, 'dude');
     player.setBounce(0.2);
     player.setOrigin(0.5, 1);
-    player.setScale(1.5);
-    //player.play('idle');
-
-    
-    //player.displayOriginX = 0;
-    //player.displayOriginY = 0;
-    //player.displayWidth = 42;
-    //player.displayHeight = 58;
-    //player.x = 0;
-    //player.y = 0
+    player.setScale(1.3);
     player.body.setGravityY(100);
-
     player.setCollideWorldBounds(true);
 
     this.anims.create({
@@ -113,42 +101,31 @@ function create() {
     cursors = this.input.keyboard.createCursorKeys();
 
     masks = this.physics.add.group({
-        key: 'mask',
-        repeat: 11,
-        setXY: { x: 20, y: 0, stepX: 80 }
+        key: 'mask', 
+        repeat: 10,
+        setXY: { x: 40, y: 0, stepX: 200 }
     });
 
     masks.children.iterate(function (child) {
-
         child.setBounceY(Phaser.Math.FloatBetween(0.1, 0.2));
 
     });
 
-    //player.health = 20;
-
-    //bombs = this.physics.add.group();
-
-    scoreText = this.add.text(16, 16, '♻️ points : 0', { fontSize: '20px', fill: '#14213d' });
+    scoreText = this.add.text(16, 16, '♻️ points : 0', { fontSize: '20px', fill: '#000' });
 
     this.physics.add.collider(player, platforms);
     this.physics.add.collider(masks, platforms);
     this.physics.add.overlap(player, masks, collectmask, null, this);
 
     this.initialTime = 0;
-    timerText = this.add.text(16, 48, '⏳ temps : 0 ', { fontSize: '20px', fill: '#14213d' });
+    timerText = this.add.text(16, 48, '⏳ temps : 0 ', { fontSize: '20px', fill: '#000'  });
     timedEvent = this.time.addEvent({ delay: 1000, callback: onEvent, callbackScope: this, loop: true });
 
-    //textGameOver = this.add.text(game.config.width / 2, game.config.height / 2, 'GAME OVER', { fontSize: '32px', fill: '#fff' });
-    textGameOver = this.add.text(400, 320, 'Game Over!', { fontSize: '48px', fill: '#14213d' })
-    //this.textGameOver.setOrigin(0.5)
+    textGameOver = this.add.text(400, 320, 'Game Over!', { fontSize: '48px', fill: '#000' })
     textGameOver.visible = false;
-
-    //textGameOver.setDepth(1);
-
-    winText = this.add.text(400, 320, 'You Win!', { fontSize: '48px', fill: '#14213d' })
+    
+    winText = this.add.image(620, 320, 'win')
     winText.visible = false;
-    //winText.setDepth(1);
-//  Event handler for when the animation completes on our sprite
 }
 
 function update() {
@@ -179,20 +156,12 @@ function update() {
 
 function collectmask(player, mask) {
     mask.disableBody(true, true);
-
     score += 10;
     scoreText.setText('♻️ points : ' + score);
 
-    if (score == 120) {
-        //this.textGameOver.visible = true;
-        //scene.scene.stop();
+    if (score == 100) {
         winText.visible = true;
         this.scene.pause();
-
-        //scoreText.visible = false;
-        //timerText.visible = false;
-        //player.setTint(0xff0000);
-        //player.anims.play('turn');
         this.gameOver = true;
     }
 
@@ -206,38 +175,14 @@ function collectmask(player, mask) {
     }
 }
 
-function startGame() {
-    //introText.visible = false;
-    scoreText.visible = true;
-    //hitPointsText.visible = true;
-    gameStarted = true;
-    finishedGame = false;
-}
-
-// End the game
-function killGame() {
-    finishedGame = true;
-    //player.setVelocity(0, 0);
-    //introText.visible = true;
-
-    //scoreText.visible = false;
-    //hitPointsText.visible = false;
-}
-
-function onEvent() {
-
-}
-
 function formatTime(seconds) {
     var minutes = Math.floor(seconds / 60);
     var partInSeconds = seconds % 60;
     return `${minutes}:${partInSeconds}`;
 }
 
-
 function onEvent() {
     if (this.initialTime === 0) {
-        //timer.paused = false;
         textGameOver.visible = false;
     }
 
